4e6a198ab1dde46ceacf9023ed38ecb51f2ee539
Josiah Lopez <josiah.lopez45@yahoo.ca>
2023-09-20 22:02:53 -0500

    Added some validations to our working models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_013138) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_023946) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "year"
