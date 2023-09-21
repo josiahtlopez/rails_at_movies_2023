a34f03086905432dba7543b4f4cc0b2139aca5b7
Josiah Lopez <josiah.lopez45@yahoo.ca>
2023-09-20 21:23:36 -0500

    Added some validations to our working models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :production_company
 
   # Requiring each field to be true or filled out
-  validates :title, :year, :duration, :description, :average_vote, :presence: true
-  validates :title, :uniqueness: true
-  validates :year, :duration, :numericality: {only_integers: true}
+  validates :title, :year, :duration, :description, :average_vote, presence: true
+  validates :title, uniqueness: true
+  validates :year, :duration, numericality: {only_integers: true}
   validates :average_vote, numericality: true
 end
