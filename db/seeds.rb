5e94d8b6c2fff7d26747c5e720607f1c226cb79e
Josiah Lopez <josiah.lopez45@yahoo.ca>
2023-09-20 23:07:07 -0500

    Left_off_point_class_sept13

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+#open
+filename = Rails.root.join("db/top_movies.csv")
+
+puts "Loading movies from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+
+movies = CSV.parse(csv_data, headers:true, encoding: "utf-8")
+
+movies.each do |m|
+  production_company = ProductionCompany.find_or_create_by(name: m['production_company'])
+
+  if production_company && production_company.valid?
+    # create the movie
+  else
+    puts "Invalid production company #{m["Production Company"]} for movie #{m['original_title']}"
+  end
+  puts m['original_title']
+end
+
+puts "Created #{ProductionCompany.count} Production Companies"
